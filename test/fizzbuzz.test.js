159a1e22d7fd6d5ef83b140d44187ac75850d29a
Nicholas Oram <nickoram@gmail.com>
2015-12-10 15:49:34 -0400

    refactor usage of fizzbuzz module
    - use case expected for module best practice

--- test/fizzbuzz.test.js
+++ test/fizzbuzz.test.js
@@ -1,37 +1,37 @@
-var FizzBuzz = require("../fizzbuzz");
+var fizzbuzz = require("../fizzbuzz");
 
 describe("FizzBuzz", function() {
-	var fizzbuzz = new FizzBuzz();
+	var f = new fizzbuzz();
 	
 	describe("divisibleBy()", function() {
         it("when divisible", function() {
-            expect(fizzbuzz.divisibleBy(3,3)).to.be.eql(true);
+            expect(f.divisibleBy(3,3)).to.be.eql(true);
         });
         
         it("when not divisable", function() {
-            expect(fizzbuzz.divisibleBy(3,2)).to.be.eql(false);
+            expect(f.divisibleBy(3,2)).to.be.eql(false);
         });
     });
 
     describe("convertToFizzBuzz()", function() {
         it("when divisible by 3", function() {
-            expect(fizzbuzz.convertToFizzBuzz(3)).to.be.eql("Fizz");
-            expect(fizzbuzz.convertToFizzBuzz(6)).to.be.eql("Fizz");
+            expect(f.convertToFizzBuzz(3)).to.be.eql("Fizz");
+            expect(f.convertToFizzBuzz(6)).to.be.eql("Fizz");
         });
 
         it("when divisible by 5", function() {
-            expect(fizzbuzz.convertToFizzBuzz(5)).to.be.eql("Buzz");
-            expect(fizzbuzz.convertToFizzBuzz(10)).to.be.eql("Buzz");
+            expect(f.convertToFizzBuzz(5)).to.be.eql("Buzz");
+            expect(f.convertToFizzBuzz(10)).to.be.eql("Buzz");
         });
 
         it("when divisible by 15", function() {
-            expect(fizzbuzz.convertToFizzBuzz(15)).to.be.eql("FizzBuzz");
-            expect(fizzbuzz.convertToFizzBuzz(30)).to.be.eql("FizzBuzz");
+            expect(f.convertToFizzBuzz(15)).to.be.eql("FizzBuzz");
+            expect(f.convertToFizzBuzz(30)).to.be.eql("FizzBuzz");
         });
 
         it("when not divisible by 3, 5, or 15", function() {
-            expect(fizzbuzz.convertToFizzBuzz(4)).to.be.eql("4");
-            expect(fizzbuzz.convertToFizzBuzz(7)).to.be.eql("7");
+            expect(f.convertToFizzBuzz(4)).to.be.eql("4");
+            expect(f.convertToFizzBuzz(7)).to.be.eql("7");
         });
     });
 });
