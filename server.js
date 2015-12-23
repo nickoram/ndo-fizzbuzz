9771c0571d45b011740a6fb2b5e3b205ee7c5fc6
Nicholas Oram <nickoram@gmail.com>
2015-12-23 13:59:24 -0400

    add route for cachefizzbuzz to server
    - misc: rename variables from snake_case to camelCase

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 var express = require("express");
 var app = express();
 var fizzbuzz = require("./fizzbuzz");
+var cachefizzbuzz = require("./cachefizzbuzz");
 
-var server_port = process.env.VCAP_APP_PORT || 8080;
-var server_host = process.env.VCAP_APP_HOST || "localhost";
+var serverPort = process.env.VCAP_APP_PORT || 8080;
+var serverHost = process.env.VCAP_APP_HOST || "localhost";
+
+var dbUrl = "";
+if (process.env.VCAP_SERVICES) {
+    var env = JSON.parse(process.env.VCAP_SERVICES);
+    dbUrl = env.cloudantNoSQLDB[0].credentials.url + "/fizzbuzz-web";
+} else {
+    dbUrl = "http://localhost:8080/fizzbuzz-web";
+}
 
 app.get("/fizzbuzz_range/:from/:to", function(request, response) {
     var f = new fizzbuzz();
@@ -17,9 +26,18 @@ app.get("/fizzbuzz_range/:from/:to", function(request, response) {
     });
 });
 
-app.listen(server_port, server_host, function() {
+app.get("/cache_fizzbuzz_range/:from/:to", function(request, response) {
+    var c = new cachefizzbuzz(dbUrl);
+    var from = request.params.from;
+    var to = request.params.to;
+
+    c.fizzBuzzRange(from, to, function(data) {
+        response.send(data);
+    });
+});
+
+app.listen(serverPort, serverHost, function() {
     var host = this.address().address;
     var port = this.address().port;
     console.log("Listening at http://%s:%s", host, port);
 });
-
